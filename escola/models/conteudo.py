5842b56e1be18c0de4d2b3f589b6716c52e8ed34
gabriel eduardo <gabrieleduardo13csc@gmail.com>
2025-09-25 21:19:21 -0300

    feat:add db models

---
+++ escola/models/conteudo.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Conteudo(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    duracao_minutos = models.PositiveIntegerField(default=60)  # duração padrão
+    descritor = models.CharField()
+
+
+    def __str__(self):
+        return self.nome
