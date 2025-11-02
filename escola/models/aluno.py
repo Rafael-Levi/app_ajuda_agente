6ed5a67a0fd7f5a1337800aa4461a0609d816aac
Rafael <rafaellevi@gmail.com>
2025-11-02 16:40:19 -0300

    Adicinando campo no schema aluno

--- escola/models/aluno.py
+++ escola/models/aluno.py
@@ -5,6 +5,7 @@ class Aluno(models.Model):
     serie = models.CharField(max_length=10, blank=True, null=False)
     turno = models.CharField(max_length=10, blank=True, null=False)
     telefone = models.CharField(max_length=20, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.nome
